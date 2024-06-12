4aef9800fccb35f6c536d08ab1e23462425aa495
Patrick Suggate <patrick.suggate@gmail.com>
2024-06-12 23:59:28 +1200

    WIP. Added initial version of a CRC32 calculator, and with the default polynomial being the same as that used by the HAL STM32G4xx libraries.

--- bench/stm32crc_tb.c
+++ bench/stm32crc_tb.c
@@ -1,3 +1,4 @@
+#include <assert.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include "stm32crc.h"
@@ -8,15 +9,17 @@ void stm32crc_tb(void)
     unsigned char p[1024];
     int32_t* q = (int32_t*)p;
 
-    printf("WIP\n");
+    printf("\nTesting CRC32 correctness (vs STM32G4xx default)\n");
 
     for (int i=256; i--;) {
 	q[i] = rand();
     }
+    assert(0 != stm32crc_calc(p, 1024));
 
-    printf("WOP: %08x\n", stm32crc_calc(p, 1024));
+    char str[] = "A test-string for CRC32 checking\n";
+    size_t len = sizeof(str) - 1; // drop the '\0'
+    uint32_t crc = stm32crc_calc((uint8_t*)str, len);
+    assert(crc == 0xf1c14ad9L);
 
-    char r[] = "salad burger potassiumate\n";
-    size_t len = sizeof(r) - 1;
-    printf("WUP: %08x (len = %lu)\n", stm32crc_calc((uint8_t*)r, len), len);
+    printf("passed\n");
 }
